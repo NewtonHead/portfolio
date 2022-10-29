3ef4c84d7130da3c8af6d05ca32e1cbb767b4d53
newton2 <newton2@github.com>
2022-10-29 03:29:30 -0300

    added animation to dropdown menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import {FaBars} from "react-icons/fa"
 
 function Navbar(props) {
+  const [className, setClassName] = useState('hidden');
+  
+  const openDropdown = () => {
+    if (className == 'hidden') {
+      setClassName('');
+      document.getElementById('navbar-list').style.animation = 'growDown 300ms ease-in-out forwards'
+    } else {
+      setClassName('hidden')
+      document.getElementById('navbar-list').style.animation = ''
+    }
+  }
+
   return (
-    <nav class="navbar">
-      <ul>
+    <nav className="navbar">
+      <span className="navbar-dropdown-button" onClick={openDropdown}><FaBars /></span>
+      <ul className={className} id="navbar-list">
         {props.data.map((data, index) =>{
           return (
-            <li className={data.className} key={index}>{data.text}</li>
+            <li className={data.className + ' noselect'} key={index}>{data.text}</li>
           )
         })}
       </ul>
