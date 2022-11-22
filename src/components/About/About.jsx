19e0a137f26db12a60ac653d090352434ff4f181
NewtonHead <NewtonHead@github.com>
2022-11-22 08:15:54 -0300

    some css refactoring

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,34 +1,34 @@
 function About() {
   return (
     <section id="about">
-      <h1 id="aboutTitle">About</h1>
+      <h1 id="aboutTitle" class="title">About</h1>
       <div className="container">
         <img src="/images/portfolio-image.jpg" alt="" srcset="" />
-        <h2 id="whoami">Who am i?</h2>
-        <p id="paragraph">My name is Santiago, i live in Argentina
+        <h2 id="whoami" class="title">Who am i?</h2>
+        <p id="paragraph" class="all-centered">My name is Santiago, i live in Argentina
         and i am an independent Front-end developer
         </p>
 
         <ul id="skills">
-          <li class="skill">
+          <li class="all-centered">
             <span class="tech">HTML</span> <span class="level">Advanced</span>
           </li>
-          <li class="skill">
+          <li class="all-centered">
             <span class="tech">CSS</span> <span class="level">Intermediate</span>
           </li>
-          <li class="skill">
+          <li class="all-centered">
             <span class="tech">Javascript</span> <span class="level">Intermediate</span>
           </li>
-          <li class="skill">
+          <li class="all-centered">
             <span class="tech">React.js</span> <span class="level">Intermediate</span>
           </li>
-          <li class="skill">
+          <li class="all-centered">
             <span class="tech">Java</span> <span class="level">Begginer</span>
           </li>
-          <li class="skill">
+          <li class="all-centered">
           <span class="tech">Python</span> <span class="level">Intermediate</span>
           </li>
-          <li class="skill">
+          <li class="all-centered">
           <span class="tech">Linux</span> <span class="level">Full-time User</span>
           </li>
         </ul>
