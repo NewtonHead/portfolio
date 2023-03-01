ef96738c5d951b260645e94077dce75c437b0c76
NewtonHead <NewtonHead@github.com>
2023-03-01 08:36:34 -0300

    added redirect link to projectList objects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,15 +3,18 @@ import ProjectCard from "./ProjectCard"
 const projectList = [
     {
         url: "https://i.imgur.com/yOsWtBf.png",
-        title: "Markdown Previewer"
+        title: "Markdown Previewer",
+        redirect: "https://codepen.io/newtonhead/pen/QWOXeLj"
     },
     {
         url: "https://i.imgur.com/fjRh9KE.png",
-        title: "Calculator"
+        title: "Calculator",
+        redirect: "https://codepen.io/newtonhead/pen/KKZoaGE"
     },
     {
         url: "https://i.imgur.com/Sb3dE1k.png",
-        title: "Drum Machine"
+        title: "Drum Machine",
+        redirect: "https://codepen.io/newtonhead/pen/jOYBdKV"
     }
 ]
 
@@ -21,7 +24,7 @@ function Projects() {
             <h2 class="title text-centered oxford-blue bold">Check some of my projects at codepen!</h2>
             {projectList.map((project, index) => {
                 return (
-                    <ProjectCard url={project.url} title={project.title} />
+                    <ProjectCard url={project.url} title={project.title} redirect={project.redirect}/>
                 )
             })}
         </section>
