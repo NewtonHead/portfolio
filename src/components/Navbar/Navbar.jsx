81e838ec373998f2e58bb51f59532ab6104714e0
NewtonHead <NewtonHead@github.com>
2023-04-09 19:46:14 -0300

    fixed an abomination

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import {FaBars} from "react-icons/fa"
 
 function Navbar(props) {
   const [hidden, isHidden] = useState(true);
+
+  const navbarList = useRef(null)
   
-  const openDropdown = () => {
+  const openDropdown = (event) => {
     if (hidden === true) {
-      isHidden(false)
-      document.getElementById('navbar-list').style.animation = 'growDown 300ms ease-in-out forwards'
-      document.getElementById('navbar-list').classList.remove('hidden')
+      isHidden(false);
+      navbarList.current.classList.remove("hidden")
+      navbarList.current.style.animation = "growDown 300ms ease-in-out forwards"
     } else {
-      isHidden(true)
-      document.getElementById('navbar-list').style.animation = ''
-      document.getElementById('navbar-list').classList.add('hidden')
+      isHidden(true);
+      navbarList.current.classList.add("hidden");
+      navbarList.current.style.animation = ""
     }
   }
 
   return (
     <nav className="navbar oxford-blue-background">
       <span className="navbar-dropdown-button almond flex-centered absolute desktop-hidden" onClick={openDropdown}><FaBars /></span>
-      <ul id="navbar-list" className="hidden absolute">
+      <ul id="navbar-list" className="hidden absolute" ref={navbarList}>
         {props.data.map((data, index) =>{
           return (
             <li key={index}>
