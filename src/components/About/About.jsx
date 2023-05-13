b21cfbbb4cceba9bfc517df544a2d40f2ef1eefe
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2023-05-13 05:21:28 -0300

    updated my desc

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,10 +9,21 @@ function About() {
     <section id="about">
       <h1 id="aboutTitle" className="title text-centered bold oxford-blue">About</h1>
       <div className="container">
-        <img src="/images/portfolio-image.jpg" alt="" srcset="" id="me"/>
+        <img src="/images/portfolio-image.jpg" alt="" srcset="" id="me" className="flex-centered"/>
         <h2 id="whoami" className="title text-centered bold oxford-blue">Who am i?</h2>
-        <p id="paragraph" className="flex-centered text-centered">My name is Santiago, i live in Argentina
-        and i am an independent Front-end developer
+        <p id="paragraph" className="flex-centered text-centered">
+          Hi! My name is Santiago, and i am a React Developer residing
+          in Argentina, i've always been interested in the design of websites
+          and tools that could solve real-world problems, so i started my coding
+          career on freecodecamp 4 years ago. <br /> <br />
+
+          I believe that the most valuable way to make an idea come true, is
+          communication, so i always try to make it my first priority with
+          my co-workers. <br /> <br />
+
+          Thank you for passing by, feel free to check all my portfolio and if
+          you are interested in working with me, please, send me a message!
+          
         </p>
 
         <ul id="skills">
